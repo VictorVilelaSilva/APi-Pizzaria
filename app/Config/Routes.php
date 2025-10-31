4a584ad804fabfa9777607684571142f67ca59d3
VictorVilela <victorsouzavilelasilva@gmail.com>
2025-10-31 14:36:56 -0300

    feat: PDB-001 - Refactor routing to include dynamic module routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -8,6 +8,11 @@ use CodeIgniter\Router\RouteCollection;
 $routes->get('/', 'Home::index');
 $routes->set404Override('App\Controllers\ErrorsController::show404');
 
-require APPPATH . 'Routes.php';
-    
+$directory = APPPATH . 'Modules';
 
+$dirIterator = new DirectoryIterator($directory);
+foreach ($dirIterator as $fileInfo) {
+    if ($fileInfo->isDir() && !$fileInfo->isDot()) {
+        require $fileInfo->getPathname() . DIRECTORY_SEPARATOR . 'Routes.php';
+    }
+}
